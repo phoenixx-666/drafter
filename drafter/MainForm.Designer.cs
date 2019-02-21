d0156bb5d2d0c6d564a49d0607c3b588dae1acc3
phoenixx-666 <gitler_ncb@mail.ru>
2019-02-22 02:38:03 +0300

    meaningful label names

--- drafter/MainForm.Designer.cs
+++ drafter/MainForm.Designer.cs
@@ -42,32 +42,32 @@ namespace drafter
 			this.c_t2b3 = new System.Windows.Forms.ComboBox();
 			this.c_t2b2 = new System.Windows.Forms.ComboBox();
 			this.c_t2b1 = new System.Windows.Forms.ComboBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
+			this.l_t1b1 = new System.Windows.Forms.Label();
+			this.l_t1b2 = new System.Windows.Forms.Label();
+			this.l_t1b3 = new System.Windows.Forms.Label();
+			this.l_t2b3 = new System.Windows.Forms.Label();
+			this.l_t2b2 = new System.Windows.Forms.Label();
+			this.l_t2b1 = new System.Windows.Forms.Label();
 			this.c_t1h1 = new System.Windows.Forms.ComboBox();
-			this.label7 = new System.Windows.Forms.Label();
-			this.label8 = new System.Windows.Forms.Label();
+			this.l_t1h1 = new System.Windows.Forms.Label();
+			this.l_t1h2 = new System.Windows.Forms.Label();
 			this.c_t1h2 = new System.Windows.Forms.ComboBox();
-			this.label9 = new System.Windows.Forms.Label();
+			this.l_t1h3 = new System.Windows.Forms.Label();
 			this.c_t1h3 = new System.Windows.Forms.ComboBox();
-			this.label10 = new System.Windows.Forms.Label();
+			this.l_t1h4 = new System.Windows.Forms.Label();
 			this.c_t1h4 = new System.Windows.Forms.ComboBox();
-			this.label11 = new System.Windows.Forms.Label();
+			this.l_t1h5 = new System.Windows.Forms.Label();
 			this.c_t1h5 = new System.Windows.Forms.ComboBox();
 			this.c_t2h1 = new System.Windows.Forms.ComboBox();
-			this.label12 = new System.Windows.Forms.Label();
+			this.l_t2h1 = new System.Windows.Forms.Label();
 			this.c_t2h2 = new System.Windows.Forms.ComboBox();
-			this.label13 = new System.Windows.Forms.Label();
+			this.l_t2h2 = new System.Windows.Forms.Label();
 			this.c_t2h3 = new System.Windows.Forms.ComboBox();
-			this.label14 = new System.Windows.Forms.Label();
+			this.l_t2h3 = new System.Windows.Forms.Label();
 			this.c_t2h4 = new System.Windows.Forms.ComboBox();
-			this.label15 = new System.Windows.Forms.Label();
+			this.l_t2h4 = new System.Windows.Forms.Label();
 			this.c_t2h5 = new System.Windows.Forms.ComboBox();
-			this.label16 = new System.Windows.Forms.Label();
+			this.l_t2h5 = new System.Windows.Forms.Label();
 			this.tResult = new System.Windows.Forms.TextBox();
 			this.bSwap = new System.Windows.Forms.Button();
 			this.bCopy = new System.Windows.Forms.Button();
@@ -140,53 +140,53 @@ namespace drafter
 			this.c_t2b1.Sorted = true;
 			this.c_t2b1.TabIndex = 8;
 			// 
-			// label1
+			// l_t1b1
 			// 
-			this.label1.Location = new System.Drawing.Point(8, 16);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(80, 16);
-			this.label1.TabIndex = 5;
-			this.label1.Text = "Ban 1";
+			this.l_t1b1.Location = new System.Drawing.Point(8, 16);
+			this.l_t1b1.Name = "l_t1b1";
+			this.l_t1b1.Size = new System.Drawing.Size(80, 16);
+			this.l_t1b1.TabIndex = 5;
+			this.l_t1b1.Text = "Ban 1";
 			// 
-			// label2
+			// l_t1b2
 			// 
-			this.label2.Location = new System.Drawing.Point(96, 16);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(80, 16);
-			this.label2.TabIndex = 6;
-			this.label2.Text = "Ban 2";
+			this.l_t1b2.Location = new System.Drawing.Point(96, 16);
+			this.l_t1b2.Name = "l_t1b2";
+			this.l_t1b2.Size = new System.Drawing.Size(80, 16);
+			this.l_t1b2.TabIndex = 6;
+			this.l_t1b2.Text = "Ban 2";
 			// 
-			// label3
+			// l_t1b3
 			// 
-			this.label3.Location = new System.Drawing.Point(184, 16);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(80, 16);
-			this.label3.TabIndex = 7;
-			this.label3.Text = "Ban 3";
+			this.l_t1b3.Location = new System.Drawing.Point(184, 16);
+			this.l_t1b3.Name = "l_t1b3";
+			this.l_t1b3.Size = new System.Drawing.Size(80, 16);
+			this.l_t1b3.TabIndex = 7;
+			this.l_t1b3.Text = "Ban 3";
 			// 
-			// label4
+			// l_t2b3
 			// 
-			this.label4.Location = new System.Drawing.Point(480, 16);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(80, 16);
-			this.label4.TabIndex = 10;
-			this.label4.Text = "Ban 3";
+			this.l_t2b3.Location = new System.Drawing.Point(480, 16);
+			this.l_t2b3.Name = "l_t2b3";
+			this.l_t2b3.Size = new System.Drawing.Size(80, 16);
+			this.l_t2b3.TabIndex = 10;
+			this.l_t2b3.Text = "Ban 3";
 			// 
-			// label5
+			// l_t2b2
 			// 
-			this.label5.Location = new System.Drawing.Point(392, 16);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(80, 16);
-			this.label5.TabIndex = 9;
-			this.label5.Text = "Ban 2";
+			this.l_t2b2.Location = new System.Drawing.Point(392, 16);
+			this.l_t2b2.Name = "l_t2b2";
+			this.l_t2b2.Size = new System.Drawing.Size(80, 16);
+			this.l_t2b2.TabIndex = 9;
+			this.l_t2b2.Text = "Ban 2";
 			// 
-			// label6
+			// l_t2b1
 			// 
-			this.label6.Location = new System.Drawing.Point(304, 16);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(80, 16);
-			this.label6.TabIndex = 8;
-			this.label6.Text = "Ban 1";
+			this.l_t2b1.Location = new System.Drawing.Point(304, 16);
+			this.l_t2b1.Name = "l_t2b1";
+			this.l_t2b1.Size = new System.Drawing.Size(80, 16);
+			this.l_t2b1.TabIndex = 8;
+			this.l_t2b1.Text = "Ban 1";
 			// 
 			// c_t1h1
 			// 
@@ -199,21 +199,21 @@ namespace drafter
 			this.c_t1h1.Sorted = true;
 			this.c_t1h1.TabIndex = 3;
 			// 
-			// label7
+			// l_t1h1
 			// 
-			this.label7.Location = new System.Drawing.Point(8, 64);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(40, 16);
-			this.label7.TabIndex = 12;
-			this.label7.Text = "Pick 1";
+			this.l_t1h1.Location = new System.Drawing.Point(8, 64);
+			this.l_t1h1.Name = "l_t1h1";
+			this.l_t1h1.Size = new System.Drawing.Size(40, 16);
+			this.l_t1h1.TabIndex = 12;
+			this.l_t1h1.Text = "Pick 1";
 			// 
-			// label8
+			// l_t1h2
 			// 
-			this.label8.Location = new System.Drawing.Point(32, 96);
-			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(40, 16);
-			this.label8.TabIndex = 14;
-			this.label8.Text = "Pick 2";
+			this.l_t1h2.Location = new System.Drawing.Point(32, 96);
+			this.l_t1h2.Name = "l_t1h2";
+			this.l_t1h2.Size = new System.Drawing.Size(40, 16);
+			this.l_t1h2.TabIndex = 14;
+			this.l_t1h2.Text = "Pick 2";
 			// 
 			// c_t1h2
 			// 
@@ -226,13 +226,13 @@ namespace drafter
 			this.c_t1h2.Sorted = true;
 			this.c_t1h2.TabIndex = 4;
 			// 
-			// label9
+			// l_t1h3
 			// 
-			this.label9.Location = new System.Drawing.Point(8, 128);
-			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(40, 16);
-			this.label9.TabIndex = 16;
-			this.label9.Text = "Pick 3";
+			this.l_t1h3.Location = new System.Drawing.Point(8, 128);
+			this.l_t1h3.Name = "l_t1h3";
+			this.l_t1h3.Size = new System.Drawing.Size(40, 16);
+			this.l_t1h3.TabIndex = 16;
+			this.l_t1h3.Text = "Pick 3";
 			// 
 			// c_t1h3
 			// 
@@ -245,13 +245,13 @@ namespace drafter
 			this.c_t1h3.Sorted = true;
 			this.c_t1h3.TabIndex = 5;
 			// 
-			// label10
+			// l_t1h4
 			// 
-			this.label10.Location = new System.Drawing.Point(32, 160);
-			this.label10.Name = "label10";
-			this.label10.Size = new System.Drawing.Size(40, 16);
-			this.label10.TabIndex = 18;
-			this.label10.Text = "Pick 4";
+			this.l_t1h4.Location = new System.Drawing.Point(32, 160);
+			this.l_t1h4.Name = "l_t1h4";
+			this.l_t1h4.Size = new System.Drawing.Size(40, 16);
+			this.l_t1h4.TabIndex = 18;
+			this.l_t1h4.Text = "Pick 4";
 			// 
 			// c_t1h4
 			// 
@@ -264,13 +264,13 @@ namespace drafter
 			this.c_t1h4.Sorted = true;
 			this.c_t1h4.TabIndex = 6;
 			// 
-			// label11
+			// l_t1h5
 			// 
-			this.label11.Location = new System.Drawing.Point(8, 192);
-			this.label11.Name = "label11";
-			this.label11.Size = new System.Drawing.Size(40, 16);
-			this.label11.TabIndex = 20;
-			this.label11.Text = "Pick 5";
+			this.l_t1h5.Location = new System.Drawing.Point(8, 192);
+			this.l_t1h5.Name = "l_t1h5";
+			this.l_t1h5.Size = new System.Drawing.Size(40, 16);
+			this.l_t1h5.TabIndex = 20;
+			this.l_t1h5.Text = "Pick 5";
 			// 
 			// c_t1h5
 			// 
@@ -294,13 +294,13 @@ namespace drafter
 			this.c_t2h1.Sorted = true;
 			this.c_t2h1.TabIndex = 11;
 			// 
-			// label12
+			// l_t2h1
 			// 
-			this.label12.Location = new System.Drawing.Point(520, 64);
-			this.label12.Name = "label12";
-			this.label12.Size = new System.Drawing.Size(40, 16);
-			this.label12.TabIndex = 12;
-			this.label12.Text = "Pick 1";
+			this.l_t2h1.Location = new System.Drawing.Point(520, 64);
+			this.l_t2h1.Name = "l_t2h1";
+			this.l_t2h1.Size = new System.Drawing.Size(40, 16);
+			this.l_t2h1.TabIndex = 12;
+			this.l_t2h1.Text = "Pick 1";
 			// 
 			// c_t2h2
 			// 
@@ -313,13 +313,13 @@ namespace drafter
 			this.c_t2h2.Sorted = true;
 			this.c_t2h2.TabIndex = 12;
 			// 
-			// label13
+			// l_t2h2
 			// 
-			this.label13.Location = new System.Drawing.Point(496, 96);
-			this.label13.Name = "label13";
-			this.label13.Size = new System.Drawing.Size(40, 16);
-			this.label13.TabIndex = 14;
-			this.label13.Text = "Pick 2";
+			this.l_t2h2.Location = new System.Drawing.Point(496, 96);
+			this.l_t2h2.Name = "l_t2h2";
+			this.l_t2h2.Size = new System.Drawing.Size(40, 16);
+			this.l_t2h2.TabIndex = 14;
+			this.l_t2h2.Text = "Pick 2";
 			// 
 			// c_t2h3
 			// 
@@ -332,13 +332,13 @@ namespace drafter
 			this.c_t2h3.Sorted = true;
 			this.c_t2h3.TabIndex = 13;
 			// 
-			// label14
+			// l_t2h3
 			// 
-			this.label14.Location = new System.Drawing.Point(520, 128);
-			this.label14.Name = "label14";
-			this.label14.Size = new System.Drawing.Size(40, 16);
-			this.label14.TabIndex = 16;
-			this.label14.Text = "Pick 3";
+			this.l_t2h3.Location = new System.Drawing.Point(520, 128);
+			this.l_t2h3.Name = "l_t2h3";
+			this.l_t2h3.Size = new System.Drawing.Size(40, 16);
+			this.l_t2h3.TabIndex = 16;
+			this.l_t2h3.Text = "Pick 3";
 			// 
 			// c_t2h4
 			// 
@@ -351,13 +351,13 @@ namespace drafter
 			this.c_t2h4.Sorted = true;
 			this.c_t2h4.TabIndex = 14;
 			// 
-			// label15
+			// l_t2h4
 			// 
-			this.label15.Location = new System.Drawing.Point(496, 160);
-			this.label15.Name = "label15";
-			this.label15.Size = new System.Drawing.Size(40, 16);
-			this.label15.TabIndex = 18;
-			this.label15.Text = "Pick 4";
+			this.l_t2h4.Location = new System.Drawing.Point(496, 160);
+			this.l_t2h4.Name = "l_t2h4";
+			this.l_t2h4.Size = new System.Drawing.Size(40, 16);
+			this.l_t2h4.TabIndex = 18;
+			this.l_t2h4.Text = "Pick 4";
 			// 
 			// c_t2h5
 			// 
@@ -370,13 +370,13 @@ namespace drafter
 			this.c_t2h5.Sorted = true;
 			this.c_t2h5.TabIndex = 15;
 			// 
-			// label16
+			// l_t2h5
 			// 
-			this.label16.Location = new System.Drawing.Point(520, 192);
-			this.label16.Name = "label16";
-			this.label16.Size = new System.Drawing.Size(40, 16);
-			this.label16.TabIndex = 20;
-			this.label16.Text = "Pick 5";
+			this.l_t2h5.Location = new System.Drawing.Point(520, 192);
+			this.l_t2h5.Name = "l_t2h5";
+			this.l_t2h5.Size = new System.Drawing.Size(40, 16);
+			this.l_t2h5.TabIndex = 20;
+			this.l_t2h5.Text = "Pick 5";
 			// 
 			// tResult
 			// 
@@ -429,32 +429,32 @@ namespace drafter
 			this.Controls.Add(this.bCopy);
 			this.Controls.Add(this.bSwap);
 			this.Controls.Add(this.tResult);
-			this.Controls.Add(this.label16);
-			this.Controls.Add(this.label11);
+			this.Controls.Add(this.l_t2h5);
+			this.Controls.Add(this.l_t1h5);
 			this.Controls.Add(this.c_t2h5);
 			this.Controls.Add(this.c_t1h5);
-			this.Controls.Add(this.label15);
-			this.Controls.Add(this.label10);
+			this.Controls.Add(this.l_t2h4);
+			this.Controls.Add(this.l_t1h4);
 			this.Controls.Add(this.c_t2h4);
 			this.Controls.Add(this.c_t1h4);
-			this.Controls.Add(this.label14);
-			this.Controls.Add(this.label9);
+			this.Controls.Add(this.l_t2h3);
+			this.Controls.Add(this.l_t1h3);
 			this.Controls.Add(this.c_t2h3);
 			this.Controls.Add(this.c_t1h3);
-			this.Controls.Add(this.label13);
-			this.Controls.Add(this.label8);
+			this.Controls.Add(this.l_t2h2);
+			this.Controls.Add(this.l_t1h2);
 			this.Controls.Add(this.c_t2h2);
 			this.Controls.Add(this.c_t1h2);
-			this.Controls.Add(this.label12);
-			this.Controls.Add(this.label7);
+			this.Controls.Add(this.l_t2h1);
+			this.Controls.Add(this.l_t1h1);
 			this.Controls.Add(this.c_t2h1);
 			this.Controls.Add(this.c_t1h1);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.l_t2b3);
+			this.Controls.Add(this.l_t2b2);
+			this.Controls.Add(this.l_t2b1);
+			this.Controls.Add(this.l_t1b3);
+			this.Controls.Add(this.l_t1b2);
+			this.Controls.Add(this.l_t1b1);
 			this.Controls.Add(this.c_t2b3);
 			this.Controls.Add(this.c_t2b2);
 			this.Controls.Add(this.c_t2b1);
@@ -471,32 +471,32 @@ namespace drafter
 		private System.Windows.Forms.Button bCopy;
 		private System.Windows.Forms.Button bSwap;
 		private System.Windows.Forms.TextBox tResult;
-		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label l_t2h5;
 		private System.Windows.Forms.ComboBox c_t2h5;
-		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label l_t2h4;
 		private System.Windows.Forms.ComboBox c_t2h4;
-		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label l_t2h3;
 		private System.Windows.Forms.ComboBox c_t2h3;
-		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label l_t2h2;
 		private System.Windows.Forms.ComboBox c_t2h2;
-		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label l_t2h1;
 		private System.Windows.Forms.ComboBox c_t2h1;
 		private System.Windows.Forms.ComboBox c_t1h5;
-		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label l_t1h5;
 		private System.Windows.Forms.ComboBox c_t1h4;
-		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label l_t1h4;
 		private System.Windows.Forms.ComboBox c_t1h3;
-		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label l_t1h3;
 		private System.Windows.Forms.ComboBox c_t1h2;
-		private System.Windows.Forms.Label label8;
-		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label l_t1h2;
+		private System.Windows.Forms.Label l_t1h1;
 		private System.Windows.Forms.ComboBox c_t1h1;
-		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label l_t2b1;
+		private System.Windows.Forms.Label l_t2b2;
+		private System.Windows.Forms.Label l_t2b3;
+		private System.Windows.Forms.Label l_t1b3;
+		private System.Windows.Forms.Label l_t1b2;
+		private System.Windows.Forms.Label l_t1b1;
 		private System.Windows.Forms.ComboBox c_t2b1;
 		private System.Windows.Forms.ComboBox c_t2b2;
 		private System.Windows.Forms.ComboBox c_t2b3;
