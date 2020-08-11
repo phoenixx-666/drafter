557d9cfe9f23cadc5b5d77c9cb1febd683272deb
phoenixx-666 <gitler_ncb@mail.ru>
2020-08-11 22:45:11 +0100

    update copyright

--- drafter/Properties/AssemblyInfo.cs
+++ drafter/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("drafter")]
-[assembly: AssemblyCopyright("Copyright 2019")]
+[assembly: AssemblyCopyright("Copyright 2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
